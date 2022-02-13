ffbdbfcb7b610c066beb7cdc4a444ac5fbea8c48
SAMULANI <mulanisaddam310@gmail.com>
2022-02-13 20:15:33 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+// import { express } from 'express';
+const express = require('express');
+const exp = express();
+
+const port = 9999;
+
+// to run this app - 
+// npm start 
+// view the output on browser at 
+// http://localhost:9999/
+// to stop - 
+// Ctrl + C
+// Y 
+
+// exp.get(arg1, arg2);
+// exp.get('/', () => {});
+
+exp.get('/', (request, response) => {
+    response.send(`Welcome to SFDC app at ${port}`);
+    console.log(`Welcome!`);
+});
+
+exp.get('/home', (request, response) => {
+    response.send(`Home page of SFDC app at ${port}`);
+    console.log(`home`);
+});
+
+exp.get('/company', (request, response) => {
+    response.send(`Company page of SFDC app at ${port}`);
+    console.log(`company`);
+});
+// exp.get('/org', (request, response) => {
+//     response.send(`Organization network of company at ${port}`);
+//     console.log(`org`);
+// });
+
+// exp.listen(arg1, arg2);
+// exp.listen(port, () => {});
+
+exp.listen(port, () => {
+    console.log(`My app is running at ${port}.`)
+});
+exp.get('/org', (request, response) => {
+    response.send(`Organization network of company at ${port}`);
+    console.log(`org`);
+});
